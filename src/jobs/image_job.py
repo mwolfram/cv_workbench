0551fc38949aa1935d72e266e3e8d2f90d836ab0
Máté Wolfram <m.wolfram@incubedit.com>
2017-09-05 13:24:28 +0000

    added sliders for luminosity

--- src/jobs/image_job.py
+++ src/jobs/image_job.py
@@ -90,14 +90,18 @@ class TrafficLightsImageJob(ImageJob):
 
     def initParameters(self):
         self.addParameter("img_number", 357, 500)
-        self.addParameter("minth", 26, 255)
-        self.addParameter("maxth", 35, 255)
+        self.addParameter("hue_lo", 26, 255)
+        self.addParameter("hue_hi", 35, 255)
+        self.addParameter("lum_lo", 0, 1000)
+        self.addParameter("lum_hi", 1000, 1000)
         self.addParameter("cch", 5, 5)
 
     def execute(self):
         img_number = self.parameters["img_number"].value
-        minth = self.parameters["minth"].value
-        maxth = self.parameters["maxth"].value
+        hue_lo = self.parameters["hue_lo"].value
+        hue_hi = self.parameters["hue_hi"].value
+        lum_lo = self.parameters["lum_lo"].value
+        lum_hi = self.parameters["lum_hi"].value
         cch = self.parameters["cch"].value
 
         imagePath = FileTools.concatenatePaths(self.imagesFolder, self.imageFileName)
@@ -127,6 +131,7 @@ class TrafficLightsImageJob(ImageJob):
         output = image.copy()
 
         # detect circles in the image
+        # int method, double dp, double minDist, double param1=100, double param2=100, int minRadius=0, int maxRadius=0
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1=30, param2=20, minRadius=5, maxRadius=30) # TODO adjustable
 
         # ensure at least some circles were found
@@ -136,8 +141,8 @@ class TrafficLightsImageJob(ImageJob):
 
             # loop over the (x, y) coordinates and radius of the circles
             for (x, y, r) in circles:
-                #if (luminosity[y,x] > 180):
-                if (hue[y,x]<maxth and hue[y,x]>minth):
-                    cv2.circle(output, (x, y), r, (255, 0, 0), 3)
+                if (luminosity[y,x]<lum_hi and luminosity[y,x]>lum_lo):
+                    if (hue[y,x]<hue_hi and hue[y,x]>hue_lo):
+                        cv2.circle(output, (x, y), r, (255, 0, 0), 3)
 
         return output
